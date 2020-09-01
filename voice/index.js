d55a1590fc4660d2953a7688ce107e3cc07d819e
Multarix <darien2105@gmail.com>
2020-09-02 01:22:54 +1000

    update

--- voice/index.js
+++ voice/index.js
@@ -1,10 +1,10 @@
 'use strict';
 
-var child_process = require('child_process');
-var path = require('path');
-var voice = exports;
+const child_process = require('child_process');
+const path = require('path');
+const voice = exports;
 const { exec } = require('child_process');
-voice.speak = function(message,rate) {
+voice.speak = function(message, rate){
 	message = message.replace(/<-/g, '|').replace(/->/g, '|').replace(/</g, '|').replace(/>/g, '|').toLowerCase();
 	exec(`powershell.exe Add-Type -AssemblyName System.speech; $speak = New-Object System.Speech.Synthesis.SpeechSynthesizer; $speak.Rate = ${rate}; [Console]::InputEncoding = [System.Text.Encoding]::UTF8; $speak.Speak([Console]::In.ReadToEnd()); exit`).stdin.end(message);
 };
