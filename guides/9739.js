bbbc177e29483b5a6c18c7ce2c88c1f6dfc9cc62
Multarix <darien2105@gmail.com>
2020-09-02 10:52:07 +1000

    No more double messages for enrage vs non-enrage

--- guides/9739.js
+++ guides/9739.js
@@ -10,78 +10,43 @@ module.exports = {
 		({ player, entity, library, effect } = dispatch.require.library);
 	},
 
+	// Needs testing?
+
 	// Kalavese (1st boss)
-	"s-739-1000-1105-0": [{ "type": "text", "sub_type": "message", "message": "Turn & Breath", "message_RU": "Поворот и дыхание" }],
-	"s-739-1000-2105-0": [{ "type": "text", "sub_type": "message", "message": "Turn & Breath", "message_RU": "Поворот и дыхание" }],
-
-	"s-739-1000-1308-0": [{ "type": "text", "sub_type": "message", "message": "In then Out", "message_RU": "В то время в" },
+	"s-739-1000-105-0": [{ "type": "text", "sub_type": "message", "message": "Turn & Breath", "message_RU": "Поворот и дыхание" }],
+	"s-739-1000-308-0": [{ "type": "text", "sub_type": "message", "message": "In then Out", "message_RU": "В то время в" },
 		{ "type": "func", "func": SpawnCircle.bind(null, false, 553, 0, 0, 10, 300, 0, 7500) }],
-	"s-739-1000-1308-1": [{ "type": "text", "sub_type": "message", "message": "Out", "message_RU": "Вне" }],
-
-	"s-739-1000-1112-0": [{ "type": "text", "sub_type": "message", "message": "Back Spray", "message_RU": "Назад пердеть" }],
-	"s-739-1000-2112-0": [{ "type": "text", "sub_type": "message", "message": "Back Spray", "message_RU": "Назад пердеть" }],
-
-	"s-739-1000-1107-0": [{ "type": "text", "sub_type": "message", "message": "Jump", "message_RU": "Прыгать" }],
-	"s-739-1000-2107-0": [{ "type": "text", "sub_type": "message", "message": "Jump", "message_RU": "Прыгать" }],
-
-	"s-739-1000-1306-0": [{ "type": "text", "sub_type": "message", "message": "Out then In", "message_RU": "Из, затем В" },
+	"s-739-1000-308-1": [{ "type": "text", "sub_type": "message", "message": "Out", "message_RU": "Вне" }],
+	"s-739-1000-112-0": [{ "type": "text", "sub_type": "message", "message": "Back Spray", "message_RU": "Назад пердеть" }],
+	"s-739-1000-107-0": [{ "type": "text", "sub_type": "message", "message": "Jump", "message_RU": "Прыгать" }],
+	"s-739-1000-306-0": [{ "type": "text", "sub_type": "message", "message": "Out then In", "message_RU": "Из, затем В" },
 		{ "type": "func", "func": SpawnCircle.bind(null, false, 553, 0, 0, 10, 300, 0, 7500) }],
 	"s-739-1000-1306-1": [{ "type": "text", "sub_type": "message", "message": "In", "message_RU": "В" }],
 
-
 	// Thormentum (2nd Boss)
-	"s-739-2000-1105-0": [{ "type": "text", "sub_type": "message", "message": "360", "message_RU": "360" },
+	"s-739-2000-105-0": [{ "type": "text", "sub_type": "message", "message": "360", "message_RU": "360" },
 		{ "type": "func", "func": SpawnCircle.bind(null, false, 553, 0, 0, 10, 300, 0, 2500) },
 		{ "type": "func", "func": SpawnCircle.bind(null, false, 553, 0, 0, 10, 510, 0, 2500) }],
-	"s-739-2000-2105-0": [{ "type": "text", "sub_type": "message", "message": "360", "message_RU": "360" },
-		{ "type": "func", "func": SpawnCircle.bind(null, false, 553, 0, 0, 10, 300, 0, 2500) },
-		{ "type": "func", "func": SpawnCircle.bind(null, false, 553, 0, 0, 10, 510, 0, 2500) }],
-	"s-739-2000-1113-0": [{ "type": "text", "sub_type": "message", "message": "Stun", "message_RU": "Оглушить" }],
-	"s-739-2000-2113-0": [{ "type": "text", "sub_type": "message", "message": "Stun", "message_RU": "Оглушить" }],
-	"s-739-2000-1108-0": [{ "type": "text", "sub_type": "message", "message": "Clense", "message_RU": "Очистить", "class_position": "heal" }],
-	"s-739-2000-2108-0": [{ "type": "text", "sub_type": "message", "message": "Clense", "message_RU": "Очистить", "class_position": "heal" }],
-	"s-739-2000-1115-0": [{ "type": "text", "sub_type": "message", "message": "Whirlwind", "message_RU": "Вихрь" },
+	"s-739-2000-113-0": [{ "type": "text", "sub_type": "message", "message": "Stun", "message_RU": "Оглушить" }],
+	"s-739-2000-108-0": [{ "type": "text", "sub_type": "message", "message": "Clense", "message_RU": "Очистить", "class_position": "heal" }],
+	"s-739-2000-115-0": [{ "type": "text", "sub_type": "message", "message": "Whirlwind", "message_RU": "Вихрь" },
 		{ "type": "func", "func": SpawnCircle.bind(null, false, 553, 0, 0, 10, 360, 0, 6500) }],
-	"s-739-2000-2115-0": [{ "type": "text", "sub_type": "message", "message": "Whirlwind", "message_RU": "Вихрь" },
-		{ "type": "func", "func": SpawnCircle.bind(null, false, 553, 0, 0, 10, 360, 0, 6500) }],
-	"s-739-2000-1119-0": [{ "type": "text", "sub_type": "message", "message": "Front", "message_RU": "Фронт" }],
-	"s-739-2000-2119-0": [{ "type": "text", "sub_type": "message", "message": "Front", "message_RU": "Фронт" }],
-	"s-739-2000-1120-0": [{ "type": "text", "sub_type": "message", "message": "Back", "message_RU": "Назад" }],
-	"s-739-2000-2120-0": [{ "type": "text", "sub_type": "message", "message": "Back", "message_RU": "Назад" }],
-	"s-739-2000-1303-0": [{ "type": "text", "sub_type": "message", "message": "Whip", "message_RU": "Кнут" }],
-	"s-739-2000-2303-0": [{ "type": "text", "sub_type": "message", "message": "Whip", "message_RU": "Кнут" }],
-
+	"s-739-2000-119-0": [{ "type": "text", "sub_type": "message", "message": "Front", "message_RU": "Фронт" }],
+	"s-739-2000-120-0": [{ "type": "text", "sub_type": "message", "message": "Back", "message_RU": "Назад" }],
+	"s-739-2000-303-0": [{ "type": "text", "sub_type": "message", "message": "Whip", "message_RU": "Кнут" }],
 
 	// Argog (3rd boss)
 	"h-739-3001-30": [{ "type": "text", "sub_type": "message", "message": "Reveal Soon", "message_RU": "Скоро появится" }],
-
-	"s-739-3000-1201-0": [{ "type": "text", "sub_type": "message", "delay": 3600, "message": 'Dodge!', "message_RU": "Уклоняйся" }],
-	"s-739-3000-2201-0": [{ "type": "text", "sub_type": "message", "delay": 3600, "message": 'Dodge!', "message_RU": "Уклоняйся" }],
-
-	"s-739-3000-1107-0": [{ "type": "text", "sub_type": "message", "message": "Many Hits", "message_RU": "Кнут" }],
-	"s-739-3000-2107-0": [{ "type": "text", "sub_type": "message", "message": "Many Hits", "message_RU": "Кнут" }],
-
-	"s-739-3000-1115-0": [{ "type": "text", "sub_type": "message", "message": 'Incoming Stun', "message_RU": "Рёв (Эвейд)!!!" },
+	"s-739-3000-201-0": [{ "type": "text", "sub_type": "message", "delay": 3650, "message": 'Dodge!', "message_RU": "Уклоняйся" }],
+	"s-739-3000-107-0": [{ "type": "text", "sub_type": "message", "message": "Many Hits", "message_RU": "Кнут" }],
+	"s-739-3000-115-0": [{ "type": "text", "sub_type": "message", "message": 'Incoming Stun', "message_RU": "Рёв (Эвейд)!!!" },
 		{ "type": "func", "func": SpawnCircle.bind(null, false, 553, 0, 0, 10, 630, 0, 4000) },
-		{ "type": "text", "sub_type": "message", "delay": 1500, "message": 'Dodge!', "message_RU": "Уклоняйся" }],
-	"s-739-3000-2115-0": [{ "type": "text", "sub_type": "message", "message": 'Incoming Stun', "message_RU": "Рёв (Эвейд)!!!" },
-		{ "type": "func", "func": SpawnCircle.bind(null, false, 553, 0, 0, 10, 630, 0, 4000) },
-		{ "type": "text", "sub_type": "message", "delay": 1500, "message": 'Dodge!', "message_RU": "Уклоняйся" }],
-
-	"s-739-3000-1118-0": [{ "type": "text", "sub_type": "message", "message": "Spin", "message_RU": "Кнут" }],
-	"s-739-3000-2118-0": [{ "type": "text", "sub_type": "message", "message": "Spin", "message_RU": "Кнут" }],
-
+		{ "type": "text", "sub_type": "message", "delay": 1800, "message": 'Dodge!', "message_RU": "Уклоняйся" }],
+	"s-739-3000-118-0": [{ "type": "text", "sub_type": "message", "message": "Spin", "message_RU": "Кнут" }],
 	// Revealed Argog
-	"s-739-3000-1167-0": [{ "type": "text", "sub_type": "message", "message": "Many Hits", "message_RU": "Кнут" }],
-	"s-739-3000-2167-0": [{ "type": "text", "sub_type": "message", "message": "Many Hits", "message_RU": "Кнут" }],
-
-	"s-739-3000-1175-0": [{ "type": "text", "sub_type": "message", "message": 'Incoming Stun', "message_RU": "Рёв (Эвейд)!!!" },
+	"s-739-3000-167-0": [{ "type": "text", "sub_type": "message", "message": "Many Hits", "message_RU": "Кнут" }],
+	"s-739-3000-175-0": [{ "type": "text", "sub_type": "message", "message": 'Incoming Stun', "message_RU": "Рёв (Эвейд)!!!" },
 		{ "type": "func", "func": SpawnCircle.bind(null, false, 553, 0, 0, 10, 630, 0, 4000) },
-		{ "type": "text", "sub_type": "message", "delay": 1500, "message": 'Dodge!', "message_RU": "Уклоняйся" }],
-	"s-739-3000-2175-0": [{ "type": "text", "sub_type": "message", "message": 'Incoming Stun', "message_RU": "Рёв (Эвейд)!!!" },
-		{ "type": "func", "func": SpawnCircle.bind(null, false, 553, 0, 0, 10, 630, 0, 4000) },
-		{ "type": "text", "sub_type": "message", "delay": 1500, "message": 'Dodge!', "message_RU": "Уклоняйся" }],
-
-	"s-739-3000-1178-0": [{ "type": "text", "sub_type": "message", "message": "Spin", "message_RU": "Кнут" }],
-	"s-739-3000-2178-0": [{ "type": "text", "sub_type": "message", "message": "Spin", "message_RU": "Кнут" }]
+		{ "type": "text", "sub_type": "message", "delay": 1800, "message": 'Dodge!', "message_RU": "Уклоняйся" }],
+	"s-739-3000-178-0": [{ "type": "text", "sub_type": "message", "message": "Spin", "message_RU": "Кнут" }]
 };
