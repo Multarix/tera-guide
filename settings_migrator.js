9699ca2b01fd8e9b199ce1606466eaa6c8aead3c
Multarix <darien2105@gmail.com>
2020-09-27 23:45:43 +1000

    Migrator changes

--- settings_migrator.js
+++ settings_migrator.js
@@ -10,8 +10,7 @@ const DefaultSettings = {
 	"rate": [2],
 	"cc": ["</font><font color=\"#ffff00\">"],
 	"language": "auto",
-	"dungeons": [],
-	"chat-name": "Guide"
+	"dungeons": {}
 };
 
 module.exports = function MigrateSettings(from_ver, to_ver, settings){
@@ -23,25 +22,38 @@ module.exports = function MigrateSettings(from_ver, to_ver, settings){
 		return DefaultSettings;
 	} else {
 		// Migrate from older version (using the new system) to latest one
-		if(from_ver + 1 < to_ver){
-			// Recursively upgrade in one-version steps
-			settings = MigrateSettings(from_ver, from_ver + 1, settings);
-
-			setTimeout(function(){
-				return MigrateSettings(from_ver + 1, to_ver, settings);
-			}, 0);
+		from_ver = parseFloat(from_ver);
+		to_ver = parseFloat(to_ver);
+		// Recursively upgrade in one-version steps
+		if(from_ver + 0.1 < to_ver){
+			settings = MigrateSettings(from_ver, from_ver + 0.1, settings);
+			return MigrateSettings(from_ver + 0.1, to_ver, settings);
 		}
 		// If we reach this point it's guaranteed that from_ver === to_ver - 1, so we can implement
 		// a switch for each version step that upgrades to the next version. This enables us to
 		// upgrade from any version to the latest version without additional effort!
+		const oldsettings = settings;
+		settings = Object.assign(DefaultSettings, {});
 		switch(to_ver){
-			default:
-				let oldsettings = settings; //eslint-disable-line
-				settings = Object.assign(DefaultSettings, {});
-				for(const option in settings){
-					if(option == "lNotice" && oldsettings["notice"]) settings[option] = oldsettings["notice"];
-					if(option == "gNotice" && oldsettings["systemNotice"]) settings[option] = oldsettings["systemNotice"];
+			case 1.12:
+				for(const option in oldsettings){
+					if(option == "dungeons"){
+						settings[option] = {};
+						let id;
+						for(const element of oldsettings[option]){
+							id = element.id;
+							delete element.id;
+							settings[option][id] = element;
+							console.log(element);
+						}
+						continue;
+					}
+					if(settings[option]){
+						settings[option] = oldsettings[option];
+					}
 				}
+				break;
+			default:
 				for(const option in oldsettings){
 					if(settings[option]){
 						settings[option] = oldsettings[option];
